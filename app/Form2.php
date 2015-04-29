80cf1ea4ca63541eab7b8766c10f0b1e6c5e3af5
Linda <sekawati.linda@gmail.com>
2015-04-29 09:37:56 +0700

    Perbaikan App

--- app/Form2.php
+++ app/Form2.php
@@ -4,6 +4,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Form2 extends Model {
 
-	//
-
+		//
+		protected fillable[
+					'telp',
+	                'email',
+	                'akta',
+	                'no_imb',
+	                'surat_tanah',
+	                'domisili',
+	                'keberatan',
+	                'pelestarian',
+	                'no_pbb',
+	                'teknis',
+	                'dbmp',
+	                'bplh'
+		];
 }
