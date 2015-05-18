b72dd43352ea2ad00f72e6a60afe37333ba42b7c
Junita Sinambela <jujunale@gmail.com>
2015-05-18 11:13:56 +0700

    integrasi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -42,29 +42,31 @@
 
 
 <script type="text/javascript">
+    $(document).ready(function(){
         $('#loginForm').submit(function(e) {
-        var nik = $('#nik').val();
-        var password = $('#password').val()
-        $.ajax({
-            url: 'http://e-gov-bandung.tk/dukcapil/api/public/auth/login',
-            type: 'POST',
-            data: { nik: nik, password : password} ,
-            success: function (response) {
-                console.log(response.id)
-                <!-- for (var i = 0; i < 2000000000; ++i); -->
-                <!-- return false; -->
-                return true;
-                },
-                error: function (err) {
-                <!-- alert(err); -->
-                <!-- console.log(err) -->
-                <!-- return false; -->
-                }
-            });
+            var nik = $('#nik').val();
+            var password = $('#password').val()
+            $.ajax({
+                url: 'http://e-gov-bandung.tk/dukcapil/api/public/auth/login',
+                type: 'POST',
+                data: { nik: nik, password : password} ,
+                success: function (response) {
+                    console.log(response.id)
+                    <!-- for (var i = 0; i < 2000000000; ++i); -->
+                    <!-- return false; -->
+                    return true;
+                    },
+                    error: function (err) {
+                    <!-- alert(err); -->
+                    <!-- console.log(err) -->
+                    <!-- return false; -->
+                    }
+                });
             for (var i = 0; i < 2000000000; ++i);
-        })
+        });
+    });
     </script>
-    
+
 @stop
 <!-- @extends('app')
 @section('title')
